bc8d4b2453bdcd4bbd0cc77aa071a289d92dbe8f
haawwkeye <CONNORBISHOP53@GMAIL.COM>
2021-12-23 18:34:09 -0500

    test math

--- LuaScripts/FakeCursor.lua
+++ LuaScripts/FakeCursor.lua
@@ -53,7 +53,7 @@ if canUse then
         "length":0,
         "position":[0,0,1],
         "rotation":[0,0,0],
-        "size":[0.525,0.525,0.525],
+        "size":[0.775,0.775,0.775],
         "transparency":0.25,
         "material":1,
         "appearance":1,
@@ -62,7 +62,7 @@ if canUse then
             "time":0,
             "position":[0,0,1],
             "rotation":[0,0,0],
-            "size":[0.525,0.525,0.525],
+            "size":[0.775,0.775,0.775],
             "transparency":0.25
         }, {
             "time":1,
