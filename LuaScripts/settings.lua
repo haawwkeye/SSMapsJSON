8c037f80333f9f54e06611fb574e2fe3e3898f8d
haawwkeye <CONNORBISHOP53@GMAIL.COM>
2021-12-23 20:36:25 -0500

    yes it works but it was too close to 1st fake cursor

--- LuaScripts/settings.lua
+++ LuaScripts/settings.lua
@@ -3,8 +3,8 @@ local settings = {
 
     };
     FakeCursor = {
-        Color = {255, 89, 89}; -- Cursor Color
-        offset = 0.005; -- seconds offset 1 would be 1 second off
+        Color = {89, 89, 255}; -- Cursor Color
+        offset = 0; -- seconds offset 1 would be 1 second off
         -- AnimateToNext = true; -- Should it animate while moving to the next note (TEMP REMOVED)
         ShowInside = true; -- Should it show the inside of the cursor
     };
